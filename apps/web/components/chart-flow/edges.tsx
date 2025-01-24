5b26b66ca7d87016c0c3b24d08bd99b7bf76b139
dbwcooper <dbwencookie@gmail.com>
2025-01-24 18:04:51 +0800

    feat: add nodes

--- apps/web/components/chart-flow/edges.tsx
+++ apps/web/components/chart-flow/edges.tsx
@@ -1,6 +1,13 @@
 import { Edge } from '@xyflow/react';
 
-export const initialEdges: Edge[] = [
-  { id: 'edge-1', source: 'node-1', sourceHandle: 'b', label: '父子', targetHandle: 't', target: 'node-2' },
-  { id: 'edge-2', source: 'node-1', sourceHandle: 'b', label: '父子', style: { stroke: 'red' }, targetHandle: 't', target: 'node-3' },
+// export const initialEdges: Edge[] = [
+//   { id: 'edge-1', source: 'node-1', sourceHandle: 'b', label: '父子', targetHandle: 't', target: 'node-2' },
+//   { id: 'edge-2', source: 'node-1', sourceHandle: 'b', label: '父子', style: { stroke: 'red' }, targetHandle: 't', target: 'node-3' },
+// ];
+
+
+export const initialEdges = [
+  { id: 'a1-a2', source: 'A-1', sourceHandle: 'b', label: '兄弟', targetHandle: 't', target: 'A-2' },
+  { id: 'a2-b', source: 'A-2', sourceHandle: 'b', label: "父子", targetHandle: 't', target: 'B' },
+  { id: 'a2-c', source: 'A-2', sourceHandle: 'b', label: "父子", targetHandle: 't', target: 'C' },
 ];
--- apps/web/components/chart-flow/nodes.tsx
+++ apps/web/components/chart-flow/nodes.tsx
@@ -1,22 +1,64 @@
 import { Node } from "@xyflow/react";
 
+// export const initialNodes: Node[] = [
+//   {
+//     id: 'node-1',
+//     type: 'nodeBasic',
+//     position: { x: 60, y: 0 },
+//     data: { value: 123, label: 'root' },
+//   },
+//   {
+//     id: 'node-2',
+//     type: 'nodeBasic',
+//     position: { x: 0, y: 80 },
+//     data: { label: 'node 2' },
+//   },
+//   {
+//     id: 'node-3',
+//     type: 'nodeBasic',
+//     position: { x: 100, y: 80 },
+//     data: { label: 'node 3' },
+//   },
+// ];
+
+
 export const initialNodes: Node[] = [
   {
-    id: 'node-1',
-    type: 'nodeBasic',
-    position: { x: 60, y: 0 },
-    data: { value: 123, label: 'root' },
+    id: 'A',
+    type: 'group',
+    position: { x: 0, y: 0 },
+    data: { label: 'A'},
+    style: {
+      width: 170,
+      height: 140,
+    },
   },
   {
-    id: 'node-2',
+    id: 'A-1',
     type: 'nodeBasic',
-    position: { x: 0, y: 80 },
-    data: { label: 'node 2' },
+    data: { label: 'A1' },
+    position: { x: 10, y: 10 },
+    parentId: 'A',
+    extent: 'parent',
   },
   {
-    id: 'node-3',
+    id: 'A-2',
     type: 'nodeBasic',
-    position: { x: 100, y: 80 },
-    data: { label: 'node 3' },
+    data: { label: 'A2' },
+    position: { x: 10, y: 90 },
+    parentId: 'A',
+    extent: 'parent',
   },
-];
+  {
+    id: 'B',
+    type: 'nodeBasic',
+    position: { x: -100, y: 200 },
+    data: { label: 'B' },
+  },
+  {
+    id: 'C',
+    type: 'nodeBasic',
+    position: { x: 100, y: 200 },
+    data: { label: 'C' },
+  },
+]
